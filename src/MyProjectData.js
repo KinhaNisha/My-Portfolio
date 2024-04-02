feeed51ccf7185928777d8e663333e8cfdc58ee0
KinhaNisha <nishakinha1411@gmail.com>
2024-04-02 17:35:24 +0530

    changes

--- src/MyProjectData.js
+++ src/MyProjectData.js
@@ -6,12 +6,6 @@ const Projects = [
         "description": "Skills Used: ReactJS, JavaScript, HTML, CSS"
     },
     {
-        "title": "Flight Game",
-        "image": "https://github.com/KinhaNisha/flying-game/blob/main/Screenshot%202024-04-02%20171221.png?raw=true",
-        "link": "https://nkspicafe.blogspot.com/2022/10/game.html",
-        "description": "Skills Used: ReactJS, JavaScript, HTML, CSS"
-    },
-    {
         "title": "Emoji Pedia",
         "image": "https://github.com/KinhaNisha/emoji-pedia/raw/main/Screenshot%202024-04-02%20142341.png",
         "link": "https://laughlo-pedia.onrender.com//",
